c9791e2dbfa65fb13578783e4557629bb43ad71e
“Alejandro” <contacto.garcia.1@gmail.com>
2021-01-23 18:20:11 -0500

    Pantalla "Home App"

---
+++ app/src/main/java/com/hache/appentrepatas/HomeActivity.java
@@ -0,0 +1,42 @@
+package com.hache.appentrepatas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Intent intent;
+    TextView login_txt, singup_txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        login_txt =(TextView) findViewById(R.id.lbl_login);
+        singup_txt = (TextView) findViewById(R.id.lbl_singup);
+
+        login_txt.setOnClickListener(this);
+        singup_txt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.lbl_login:
+                intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.lbl_singup:
+                intent = new Intent(this, SingupActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}

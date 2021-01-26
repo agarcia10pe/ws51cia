05a58c729b43dc06a3edc06f613c5c11773159cc
“Paulo” <noronha.paulo25@gmail.com>
2021-01-25 21:30:38 -0500

    Subiendo archivos

--- app/src/main/java/com/hache/appentrepatas/bean/Adopt.java
+++ app/src/main/java/com/hache/appentrepatas/bean/Adopt.java
@@ -3,6 +3,7 @@ package com.hache.appentrepatas.bean;
 public class Adopt {
     private String foto;
     private String nombre;
+    private String estado;
 
     public String getFoto() {
         return foto;
@@ -18,5 +19,13 @@ public class Adopt {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 }

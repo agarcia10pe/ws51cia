33d6e2f3d22f4435fb4da91fe803922e55d25c86
Alejandro <contacto.garcia.1@gmail.com>
2021-02-16 23:17:11 -0500

    Eliminar VeterinariaDbHelper.java

--- app/src/main/java/com/hache/appentrepatas/SingupActivity.java
+++ app/src/main/java/com/hache/appentrepatas/SingupActivity.java
@@ -15,8 +15,6 @@ import android.widget.Toast;
 import com.hache.appentrepatas.dto.BaseResponse;
 import com.hache.appentrepatas.dto.ClienteRequest;
 import com.hache.appentrepatas.helper.SharedPreferencesManager;
-import com.hache.appentrepatas.helper.VeterinariaContract;
-import com.hache.appentrepatas.helper.VeterinariaDbHelper;
 import com.hache.appentrepatas.http.SolicitudClient;
 import com.hache.appentrepatas.http.SolicitudService;
 import com.hache.appentrepatas.util.Constants;
@@ -33,9 +31,7 @@ public class SingupActivity extends AppCompatActivity implements View.OnClickLis
     Intent intent;
     EditText correo, password, repassword, nombre, apellido, telefono;
     ProgressDialog progressDialog;
-    VeterinariaDbHelper dbHelper;
     SolicitudService solicitudService;
-    long newRowId = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +40,6 @@ public class SingupActivity extends AppCompatActivity implements View.OnClickLis
         initView();
         initRetrofit();
         initProgressDialog();
-        dbHelper = new  VeterinariaDbHelper(this);
-        //FeedReaderDbHelper dbHelper = new FeedReaderDbHelper(getContext());
     }
 
     private void initView() {
@@ -167,28 +161,5 @@ public class SingupActivity extends AppCompatActivity implements View.OnClickLis
                 Toast.makeText(getApplicationContext(), R.string.mensaje_error_conexion, Toast.LENGTH_SHORT).show();
             }
         });
-    }
-    void crearUsuario() {
-
-        try{
-            SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-            ContentValues values = new ContentValues();
-            values.put(VeterinariaContract.UsuarioEntry.COLUMN_NAME_CORREO, correo.getText().toString().trim());
-            values.put(VeterinariaContract.UsuarioEntry.COLUMN_NAME_PASSWORD, password.getText().toString().trim());
-            values.put(VeterinariaContract.UsuarioEntry.COLUMN_NAME_TIPO_USUARIO, "1");
-            values.put(VeterinariaContract.UsuarioEntry.COLUMN_NAME_NOMBRE, nombre.getText().toString().trim());
-            values.put(VeterinariaContract.UsuarioEntry.COLUMN_NAME_PATERNO, apellido.getText().toString().trim());
-            values.put(VeterinariaContract.UsuarioEntry.COLUMN_NAME_CELULAR, telefono.getText().toString().trim());
-            values.put(VeterinariaContract.UsuarioEntry.COLUMN_NAME_ESTADO, "1");
-// Insert the new row, returning the primary key value of the new row
-            newRowId = db.insert(VeterinariaContract.UsuarioEntry.TABLE_NAME, null, values);
-        }
-        catch (Exception ex){
-            System.out.println("Error de registro SQLLite ");
-            System.out.println("Mensaje: "+ ex.getMessage());
-            Toast.makeText(this, getString(R.string.msg_error_ocurrio),Toast.LENGTH_LONG).show();
-        }
-
     }
 }
